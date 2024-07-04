1a953e88c0233be8905557fd7a061474848fbcc8
Eryk Wójcik <drumpixtv@gmail.com>
2024-07-04 18:27:20 +0200

    db setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 
 const mockUrls = [
@@ -14,11 +15,21 @@ const mockImages = mockUrls.map((url, index) => ({
 }));
 
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  console.log("posts")
+  console.log(posts);
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages, ...mockImages].map((image) => (
+        {posts.map((post) => (  
+          <div key={post.id} className="w-48">
+            {post.name}
+          </div>  
+        ))}
+
+        {mockImages.map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url} alt="" className="w-full" />
           </div>
