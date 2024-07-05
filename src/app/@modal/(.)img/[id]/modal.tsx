39f33f382512a19ef63c5ae37d1b56f7823c81c2
Eryk Wójcik <drumpixtv@gmail.com>
2024-07-05 11:26:17 +0200

    changed background color

--- src/app/@modal/(.)img/[id]/modal.tsx
+++ src/app/@modal/(.)img/[id]/modal.tsx
@@ -21,7 +21,7 @@ export function Modal({ children }: { children: React.ReactNode }) {
   return createPortal(
     <dialog
       ref={dialogRef}
-      className="m-0 h-screen w-screen bg-zinc-800/50 text-zinc-50"
+      className="m-0 h-screen w-screen bg-black/70 text-zinc-50"
       onClose={onDismiss}
     >
       {children}
--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -4,15 +4,14 @@ export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
 
   return (
-    <div className="flex h-full w-full min-w-0 ">
+    <div className="flex h-full w-full min-w-0 min-h-0">
       {" "}
       <div className="flex flex-shrink items-center justify-center">
         <img src={image?.url} className="flex-shrink" alt="" />
       </div>
       <div className="flex w-48 flex-shrink-0 flex-col ">
-        {" "}
-        <div className="text.xl font-bold">{image?.name} </div>
-      </div>{" "}
+        <div className="p-2 text-center text-xl font-bold">{image?.name} </div>
+      </div>
     </div>
   );
 }
