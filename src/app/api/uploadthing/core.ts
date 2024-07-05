481c038fcc83d449e6708fc579545911ff146580
Eryk Wójcik <drumpixtv@gmail.com>
2024-07-05 14:02:30 +0200

    added upload permissions

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,6 +1,6 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import { images } from "~/server/db/schema";
 import { db } from "~/server/db";
 import { ratelimit } from "~/server/ratelimit";
@@ -11,8 +11,12 @@ export const ourFileRouter = {
   imageUploader: f({ image: { maxFileSize: "16MB", maxFileCount: 50 } })
     .middleware(async ({ req }) => {
       const user = auth();
-
       if (!user.userId) throw new UploadThingError("Unauthorized");
+
+      const fullUserData = await clerkClient.users.getUser(user.userId);
+
+      if (fullUserData?.privateMetadata?.["admin"] !== true)
+        throw new UploadThingError("User cannot upload images");
 
       const { success } = await ratelimit.limit(user.userId);
       if (!success) throw new UploadThingError("Rate limited");
@@ -20,7 +24,6 @@ export const ourFileRouter = {
       return { userId: user.userId };
     })
     .onUploadComplete(async ({ metadata, file }) => {
-
       await db.insert(images).values({
         name: file.name,
         url: file.url,
