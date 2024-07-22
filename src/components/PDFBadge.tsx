6d50113396a2307a5487e684e35502fcabdfc8e2
Eryk Wójcik <drumpixtv@gmail.com>
2024-07-22 18:11:40 +0200

    updated an icon on badge

--- src/components/PDFBadge.tsx
+++ src/components/PDFBadge.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { Badge } from "~/components/ui/badge";
 
-function LinkIcon() {
+function PDFIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -14,36 +14,28 @@ function LinkIcon() {
       <path
         strokeLinecap="round"
         strokeLinejoin="round"
-        d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+        d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25"
       />
     </svg>
   );
 }
 
-function PDFIcon() {
+export function PDFLinkBadge({
+  href,
+  badge,
+}: {
+  href: string;
+  badge?: string;
+}) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="size-3"
+    <Link
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="flex gap-1"
     >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
-      />
-    </svg>
-  );
-}
-
-export function PDFLinkBadge({ href, badge }: { href: string; badge?: string }) {
-  return (
-    <Link href={href} target="_blank" rel="noopener noreferrer" className="flex gap-1">
       {badge && (
-        <Badge className="hover:bg-primary/80 transition-colors">
+        <Badge className="transition-colors hover:bg-primary/80">
           <div className="flex flex-row items-center gap-1 tracking-wider">
             {badge}
             <PDFIcon />
