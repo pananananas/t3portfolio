f07cadeea8191c3dd5f076ba8429da58271f9bc2
Eryk Wójcik <drumpixtv@gmail.com>
2024-07-22 14:34:34 +0200

    updated input bg

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "text-md flex h-10 w-full rounded-md border border-input bg-[rgba(10,10,10,0.5)] px-3 py-2 ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 sm:text-sm",
+          "text-md flex h-10 w-full rounded-md border border-input bg-[rgba(231,231,231,0.15)] px-3 py-2 ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 sm:text-sm",
           className,
         )}
         ref={ref}
--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -10,7 +10,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "text-md flex max-h-[500px] min-h-[80px] w-full rounded-md border border-input bg-[rgba(10,10,10,0.5)] px-3 py-2 ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring- focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 sm:text-sm",
+          "text-md flex max-h-[500px] min-h-[80px] w-full rounded-md border border-input bg-[rgba(231,231,231,0.15)] px-3 py-2 ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring- focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 sm:text-sm",
           className,
         )}
         ref={ref}
